4a9b334b980f9b2ffb8b394034ec6ab5381a753a
ant07hony <ant07hony@yahoo.com>
2023-05-06 14:12:36 -0400

    journal controller, models and routes created

---
+++ backend/controllers/journal-controller.js
@@ -0,0 +1,66 @@
+const express = require("express")
+const {Journal} = require("../models")
+const { options } = require("../routes/journal-router")
+
+// JOURNAL INDEX ACTION
+async function index(req, res, next){
+    try {
+        // get all journals
+        res.json(await Journal.find({}))
+
+    }catch(error){
+        res.status(400).json(error)
+    }
+}
+
+// JOURNAL CREATE ACTION
+async function create(req, res, next){
+    try {
+        // create new journal
+        res.json(await Journal.create(req.body))
+
+    }catch(error){
+        res.status(400).json(error)
+    }
+}
+
+// JOURNAL SHOW ACTION
+async function detail(req, res, next){
+    try {
+        // send one journal
+        res.json(await Journal.findById(req.params.id))
+
+    }catch(error){
+        res.status(400).json(error)
+    }
+}
+
+// JOURNAL UPDATE ACTION
+async function update(req, res, next){
+    try {
+        // send one journal
+        res.json(await Journal.findByIdAndUpdate(req.params.id, req.body, {new:true}))
+
+    }catch(error){
+        res.status(400).json(error)
+    }
+}
+
+// JOURNAL DESTROY ACTION
+async function destroy(req, res, next){
+    try {
+        // send one journal
+        res.json(await Journal.findByIdAndRemove(req.params.id))
+
+    }catch(error){
+        res.status(400).json(error)
+    }
+}
+
+module.exports = {
+    index,
+    create,
+    getOne: detail,
+    update,
+    delete: destroy
+}
--- backend/routes/journal-router.js
+++ backend/routes/journal-router.js
@@ -1,32 +1,23 @@
 // DEPENDENCIES
 const express = require("express")
 const router = express.Router()
+const journalCtrl = require("../controllers/journal-controller")
 
 // ROUTES
 
 // JOURNAL INDEX ROUTE
-router.get('/', async (req,res) => {
-    res.status(200).json({ message: "journal index route" })
-})
+router.get('/', journalCtrl.index)
 
 // JOURNAL CREATE ROUTE
-router.post('/', async (req,res) => {
-    res.status(200).json({ message: "journal create route"})
-})
+router.post('/', journalCtrl.create)
 
 // JOURNAL SHOW(detail) ROUTE
-router.get('/:id', async (req,res) => {
-    res.status(200).json({ message: "journal show route: " + req.params.id})
-})
+router.get('/:id', journalCtrl.getOne)
 
 // JOURNAL UPDATE ROUTE
-router.put('/:id', async (req,res) => {
-    res.status(200).json({ message: "journal show route: " + req.params.id})
-})
+router.put('/:id', journalCtrl.update)
 
 // JOURNAL DELETE ROUTE
-router.delete('/:id', async (req,res) => {
-    res.status(200).json({ message: "journal show route: " + req.params.id})
-})
+router.delete('/:id', journalCtrl.delete)
 
 module.exports = router
